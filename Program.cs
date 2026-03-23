928d7c5a1f8a86a63c8964be215a3b42153ed26a
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-23 20:32:06 +0100

    update variable name

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@
 Console.WriteLine("Welcome to the .Net App Database Setup assistant"); 
 // Database options
 Console.WriteLine("Choose which database you like to connect to: MsSql (1), MySql (2), PostgresSql (3)");
-string? dbName = Console.ReadLine();
-if (String.IsNullOrEmpty(dbName))
+
+string? dbNameUserInput = Console.ReadLine();
+
+if (String.IsNullOrEmpty(dbNameUserInput))
 {
     Console.WriteLine("Please enter a valid database option");
 }
