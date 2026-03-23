d65ec1daadf2dd9e1689b0c81b7d665c7554b930
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-23 20:16:21 +0100

    Add database options

--- Program.cs
+++ Program.cs
@@ -2,3 +2,10 @@
 // Console Application , .NET App Database setup assistant
 // Welcome message
 Console.WriteLine("Welcome to the .Net App Database Setup assistant"); 
+// Database options
+Console.WriteLine("Choose which database you like to connect to: MsSql (1), MySql (2), PostgresSql (3)");
+string? dbName = Console.ReadLine();
+if (String.IsNullOrEmpty(dbName))
+{
+    Console.WriteLine("Please enter a valid database option");
+}
