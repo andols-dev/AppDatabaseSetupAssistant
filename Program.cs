da2667a894a3895ed2490398b9c85979764fc6c8
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-23 20:50:30 +0100

    add while loop

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 ﻿
 // Console Application , .NET App Database setup assistant
+// variables
+string dbName = "";
 // Welcome message
 Console.WriteLine("Welcome to the .Net App Database Setup assistant"); 
-// Database options
-Console.WriteLine("Choose which database you like to connect to: MsSql (1), MySql (2), PostgresSql (3)");
-
-string? dbNameUserInput = Console.ReadLine();
-
-if (String.IsNullOrEmpty(dbNameUserInput))
+while (true)
 {
-    Console.WriteLine("Please enter a valid database option");
+    // Database options
+    Console.WriteLine("Choose which database you like to connect to: MsSql (1), MySql (2), PostgresSql (3), Exit (q)");
 }
