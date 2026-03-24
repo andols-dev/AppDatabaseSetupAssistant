8f248f854ecc8d5e75e6747e19fee47feff3f743
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-24 18:20:15 +0100

    refactor

--- Program.cs
+++ Program.cs
@@ -15,65 +15,9 @@ Console.WriteLine($"Using {dbOption}");
 // connection string
 if (dbOption == "MsSql")
 {
-    while (true)
-    {
-        Console.WriteLine($"In order to create a connection string for a {dbOption} database you need to answer the following questions");
-
-        Console.WriteLine("What do you want to name the connection string?");
-        string? connString = Console.ReadLine()?.Trim();
-        if (String.IsNullOrEmpty(connString))
-        {
-            Console.WriteLine("Please enter a valid connection string");
-            continue;
-        }
-
-        Console.WriteLine("What is the server name?");
-        string? serverName = Console.ReadLine()?.Trim();
-
-        if (String.IsNullOrEmpty(serverName))
-        {
-            Console.WriteLine("Please enter a valid server name");
-            continue;
-        }
-        Console.WriteLine($"Create the name for the database");
-        string? databaseName = Console.ReadLine()?.Trim();
-        if (String.IsNullOrEmpty(databaseName))
-        {
-            Console.WriteLine("Please enter a valid database name");
-            continue;
-        }
-        Console.WriteLine("Do you want to add \"TrustServerCertificate=True\" (y/n)");
-        string? serverCertificate = Console.ReadLine()?.Trim();
-        if (String.IsNullOrEmpty(serverCertificate))
-        {
-            Console.WriteLine("Please enter a valid option: y or n");
-            continue;
-        }
-        Console.WriteLine("Do you want to add \"MultipleActiveResultSets=True\" (y/n)");
-        string? activeResultSets = Console.ReadLine()?.Trim();
-        if (String.IsNullOrEmpty(activeResultSets))
-        {
-            Console.WriteLine("Please enter a valid option: y or n");
-            continue;
-        }
-        Console.WriteLine("Do you want to add \"Trusted_Connection=True\" (y/n)");
-        string? trustedConnection = Console.ReadLine()?.Trim();
-        if (String.IsNullOrEmpty(trustedConnection))
-        {
-            Console.WriteLine("Please enter a valid option: y or n");
-            continue;
-        }
-
-        string trustCert = serverCertificate?.ToLower() == "y" ? "TrustServerCertificate=True;" : "";
-        string trustConn = trustedConnection?.ToLower() == "y" ? "Trusted_Connection=True;" : "";
-        string mars = activeResultSets?.ToLower() == "y" ? "MultipleActiveResultSets=True;" : "";
-        string connectionString = $"\"Server={serverName};Database={databaseName};{trustCert}{trustConn}{mars}\"";
-        Console.WriteLine($"Your connection string is: {connectionString}");
-        break;
-    }
-
+    string connectionString = CreateString(dbOption);
+    Console.WriteLine($"Generated connection string: {connectionString}");
 }
-
 
 static string GetDatabaseChoice()
 {
@@ -112,6 +56,59 @@ static string GetDatabaseChoice()
 }
 
 
+static string CreateString(string dbOption)
+{
+    while (true)
+    {
+        Console.WriteLine($"In order to create a connection string for a {dbOption} database you need to answer the following questions");
+
+        Console.WriteLine("What do you want to name the connection string?");
+        string? connString = Console.ReadLine()?.Trim();
+        if (String.IsNullOrEmpty(connString))
+        {
+            Console.WriteLine("Please enter a valid connection string");
+            continue;
+        }
+
+        Console.WriteLine("What is the server name?");
+        string? serverName = Console.ReadLine()?.Trim();
+
+        if (String.IsNullOrEmpty(serverName))
+        {
+            Console.WriteLine("Please enter a valid server name");
+            continue;
+        }
+        Console.WriteLine($"Create the name for the database");
+        string? databaseName = Console.ReadLine()?.Trim();
+        if (String.IsNullOrEmpty(databaseName))
+        {
+            Console.WriteLine("Please enter a valid database name");
+            continue;
+        }
+        string serverCertificate = GetYesNoInput("Do you want to add \"TrustServerCertificate=True\" (y/n)");
+        string activeResultSets = GetYesNoInput("Do you want to add \"MultipleActiveResultSets=True\" (y/n)");
+        string trustedConnection = GetYesNoInput("Do you want to add \"Trusted_Connection=True\" (y/n)");
+
+        string trustCert = serverCertificate == "y" ? "TrustServerCertificate=True;" : "";
+        string trustConn = trustedConnection == "y" ? "Trusted_Connection=True;" : "";
+        string mars = activeResultSets == "y" ? "MultipleActiveResultSets=True;" : "";
+        string connectionString = $"\"Server={serverName};Database={databaseName};{trustCert}{trustConn}{mars}\"";
+        return connectionString;
+    }
+
+}
+
+static string GetYesNoInput(string prompt)
+{
+    while (true)
+    {
+        Console.WriteLine(prompt);
+        string? input = Console.ReadLine()?.Trim();
+        if (input?.ToLower() is "y" or "n")
+            return input;
+        Console.WriteLine("Please enter a valid option: y or n");
+    }
+}
 
 // implementation for PostgresSql can be added here
 /* else if (dbOption == "PostgresSql")
