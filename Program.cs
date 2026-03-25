0ebb48c972e271596080bc6fe11e9c935c4280c4
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-25 14:51:34 +0100

    move logic to a  seperate class

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿// Console Application , .NET App Database setup assistant
-// variables
+﻿using AppDatabaseSetupAssistant;
 
-// Welcome message
 Console.WriteLine("Welcome to the .Net App Database Setup assistant");
 string? dbOption = GetDatabaseChoice();
 if (dbOption == null)
@@ -11,11 +9,9 @@ if (dbOption == null)
 }
 Console.WriteLine($"Using {dbOption}");
 
-// Create connection string based on chosen option
-// connection string
 if (dbOption == "MsSql")
 {
-    string connectionString = CreateString(dbOption);
+    string connectionString = DatabaseAssistant.CreateString(dbOption);
     Console.WriteLine($"Generated connection string: {connectionString}");
 }
 
@@ -23,9 +19,7 @@ static string GetDatabaseChoice()
 {
     while (true)
     {
-        // Database options
         Console.WriteLine("Choose which database you like to connect to: MsSql (1), PostgresSql (2), Exit (q)");
-        // user input
         string? dbOptionUserInput = Console.ReadLine()?.Trim();
 
         if (String.IsNullOrEmpty(dbOptionUserInput))
@@ -54,69 +48,3 @@ static string GetDatabaseChoice()
         return dbOption;
     }
 }
-
-
-static string CreateString(string dbOption)
-{
-    while (true)
-    {
-        Console.WriteLine($"In order to create a connection string for a {dbOption} database you need to answer the following questions");
-
-        Console.WriteLine("What do you want to name the connection string?");
-        string? connString = Console.ReadLine()?.Trim();
-        if (String.IsNullOrEmpty(connString))
-        {
-            Console.WriteLine("Please enter a valid connection string");
-            continue;
-        }
-
-        Console.WriteLine("What is the server name?");
-        string? serverName = Console.ReadLine()?.Trim();
-
-        if (String.IsNullOrEmpty(serverName))
-        {
-            Console.WriteLine("Please enter a valid server name");
-            continue;
-        }
-        Console.WriteLine($"Create the name for the database");
-        string? databaseName = Console.ReadLine()?.Trim();
-        if (String.IsNullOrEmpty(databaseName))
-        {
-            Console.WriteLine("Please enter a valid database name");
-            continue;
-        }
-        string serverCertificate = GetYesNoInput("Do you want to add \"TrustServerCertificate=True\" (y/n)");
-        string activeResultSets = GetYesNoInput("Do you want to add \"MultipleActiveResultSets=True\" (y/n)");
-        string trustedConnection = GetYesNoInput("Do you want to add \"Trusted_Connection=True\" (y/n)");
-
-        string trustCert = serverCertificate == "y" ? "TrustServerCertificate=True;" : "";
-        string trustConn = trustedConnection == "y" ? "Trusted_Connection=True;" : "";
-        string mars = activeResultSets == "y" ? "MultipleActiveResultSets=True;" : "";
-        string connectionString = $"\"Server={serverName};Database={databaseName};{trustCert}{trustConn}{mars}\"";
-        return connectionString;
-    }
-
-}
-
-static string GetYesNoInput(string prompt)
-{
-    while (true)
-    {
-        Console.WriteLine(prompt);
-        string? input = Console.ReadLine()?.Trim();
-        if (input?.ToLower() is "y" or "n")
-            return input;
-        Console.WriteLine("Please enter a valid option: y or n");
-    }
-}
-
-// implementation for PostgresSql can be added here
-/* else if (dbOption == "PostgresSql")
-{
-    Console.WriteLine($"In order to create a connection string for a {dbOption} database you need to answer the following questions");
-    string? host = Console.ReadLine()?.Trim();
-    Console.WriteLine($"Create the name for the database");
-    string? databaseName = Console.ReadLine()?.Trim();
-    string? username = Console.ReadLine()?.Trim();
-    string? password = Console.ReadLine()?.Trim();
-} */
