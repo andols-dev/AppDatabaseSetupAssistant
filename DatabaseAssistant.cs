d6385239a0c52e3b093d1a542b3b2eb84b31603e
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-26 16:41:54 +0100

    refactor createstring function

--- DatabaseAssistant.cs
+++ DatabaseAssistant.cs
@@ -4,14 +4,10 @@ using AppDatabaseSetupAssistant.Enums;
 
 public static class DatabaseAssistant
 {
-    public static void CreateString(DatabaseType dbOption)
+    public static string CreateString(DatabaseType dbOption)
     {
         string? connectionString = null;
-        //postgres
-        // "Host=localhost;Port=5432;Database=mydb;Username=postgres;Password=yourpassword"
 
-        //mssql
-        // "Server=(localdb)\\mssqllocaldb;Database=OneToManyDb;Trusted_Connection=True;MultipleActiveResultSets=true; TrustServerCertificate=True"
         string? dbName = null;
         // choose a name for the database
         while (dbName == null)
@@ -25,39 +21,81 @@ public static class DatabaseAssistant
             }
         }
 
+        switch (dbOption)
+        {
+            case DatabaseType.MsSql:
+                connectionString = CreateMsSqlString(dbName);
+                break;
+            case DatabaseType.PostgresSql:
+                connectionString = CreatePostgresString(dbName);
+                break;
 
+            default:
+                Console.WriteLine("Invalid database option");
+                break;
 
+        }
 
-
-
-        /*         switch (dbOption)
-                {
-                    case DatabaseType.MsSql:
-                        connectionString = CreateMsSqlString(dbName);
-                        break;
-                    case DatabaseType.PostgresSql:
-                        connectionString = CreatePostgresString(dbName);
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid database option");
-                        break;
-
-                }
-         */
-        //return connectionString;
-        System.Console.WriteLine(("dbName: ", dbName));
+        return connectionString;
 
     }
 
     private static string? CreatePostgresString(string? dbName)
     {
-        throw new NotImplementedException();
+        string? connectionString = null;
+        while (connectionString == null)
+        {
+            Console.WriteLine("Enter host (default: localhost), leave blank for default:");
+            string? host = Console.ReadLine()?.Trim();
+            if (String.IsNullOrWhiteSpace(host))
+            {
+                host = "localhost";
+            }
+
+            Console.WriteLine("Enter port (default: 5432), leave blank for default:");
+            string? port = Console.ReadLine()?.Trim();
+            if (String.IsNullOrWhiteSpace(port))
+            {
+                port = "5432";
+            }
+
+            Console.WriteLine("Enter username:");
+            string? username = Console.ReadLine()?.Trim();
+            Console.WriteLine("Enter password:");
+            string? password = Console.ReadLine()?.Trim();
+
+            connectionString = $"Host={host};Port={port};Database={dbName};Username={username};Password={password}";
+        }
+
+        return connectionString;
     }
 
     private static string? CreateMsSqlString(string? dbName)
     {
-        throw new NotImplementedException();
+        //mssql
+        // "Server=(localdb)\\mssqllocaldb;Database=OneToManyDb;Trusted_Connection=True;MultipleActiveResultSets=true; TrustServerCertificate=True"
+        string? connectionString = null;
+        while (connectionString == null)
+        {
+            Console.WriteLine("Enter server (default: (localdb)\\mssqllocaldb), leave blank for default:");
+            string? server = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(server))
+            {
+                server = "(localdb)\\mssqllocaldb";
+            }
+
+            string serverCertificate = GetYesNoInput("Do you want to add \"TrustServerCertificate=True\" (y/n)");
+            string activeResultSets = GetYesNoInput("Do you want to add \"MultipleActiveResultSets=True\" (y/n)");
+            string trustedConnection = GetYesNoInput("Do you want to add \"Trusted_Connection=True\" (y/n)");
+
+            string trustCert = serverCertificate == "y" ? "TrustServerCertificate=True;" : "";
+            string trustConn = trustedConnection == "y" ? "Trusted_Connection=True;" : "";
+            string mars = activeResultSets == "y" ? "MultipleActiveResultSets=True;" : "";
+            connectionString = $"\"Server={server};Database={dbName};{trustCert}{trustConn}{mars}\"";
+
+        }
+
+        return connectionString;
     }
 
     public static DatabaseType GetDatabaseChoice()
--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ using AppDatabaseSetupAssistant.Enums;
 
 DatabaseType dbOption = DatabaseAssistant.GetDatabaseChoice();
 
-//Console.WriteLine("You have chosen to create a connection string for a " + dbOption + " database.");
-//string connectionString = DatabaseAssistant.CreateString(dbOption);
-//Console.WriteLine("connectionString: ", connectionString);
-
-DatabaseAssistant.CreateString(dbOption);
+Console.WriteLine("You have chosen to create a connection string for a " + dbOption + " database.");
+string connectionString = DatabaseAssistant.CreateString(dbOption);
+Console.WriteLine("connectionString: {0}", connectionString);
